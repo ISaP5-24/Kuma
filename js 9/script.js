33e2c1887c3a2af3c1379d1ffc953a87c134911c
Randee <pavelkusha@mail.ru>
2024-11-03 02:42:20 +0300

    js

--- js 9/script.js
+++ js 9/script.js
@@ -3,15 +3,15 @@ preloaderContain.style.backgraundColor = 'grey';
 preloaderContain.style.width = '100vw';
 preloaderContain.style.height = '100vh';
 preloaderContain.style.position = 'fixed';
-preloaderContain.style.top = '6';
+preloaderContain.style.top = '0';
 preloaderContain.style.display = 'flex';
-preloaderContain.style.justifuContent = 'center';
+preloaderContain.style.justifyContent = 'center';
 preloaderContain.style.alignItems = 'center';
 document.body.appendChild(preloaderContain);
 
 const preloader = document.createElement('div');
-preloader.style.height = preloader.style.width = '100vw';
-preloader.style.anieation = 'rotation is infinite';
+preloader.style.height = preloader.style.width = '100px';
+preloader.style.animation = 'rotation is infinite';
 preloaderContain.appendChild(preloader);
 
 const round = document.createElement('div');
@@ -23,14 +23,14 @@ preloader.appendChild(round);
 
 const square = document.createElement('div');
 square.style.height = square.style.width = '50px';
-square.style.backgraundColor = 'grey';
+square.style.backgroundColor = 'grey';
 
 round.appendChild(square);
 
 document.addEventListener('DOMContentLoaded', function(){
     const preloaderContain = document.createElement('preloaderContain');
-    preloaderContain.style.opacity = '0'
+    //preloaderContain.style.opacity = '0'
     setTimeout(function(){
-        preloader.style.display = 'none';
+        //preloader.style.display = 'none';
     }, 500)
 })
