9d6012354ef431db0b7ee66da726f933b4b1fb25
Randee <pavelkusha@mail.ru>
2024-11-03 04:15:58 +0300

    gs
    	modified:   "work \342\204\2269/scripts.js"
    	modified:   "work \342\204\2269/index.php"
    	modified:   "work \342\204\2269/scripts.js"

--- work №9/scripts.js
+++ work №9/scripts.js
@@ -1,17 +1,19 @@
 const preloaderContain = document.createElement('div');
-preloaderContain.style.backgroundColor = 'grey';
+preloaderContain.style.backgroundColor = 'black';
 preloaderContain.style.width = '100vw';
 preloaderContain.style.height = '100vh';
 preloaderContain.style.position = 'fixed';
-preloaderContain.style.top = preloaderContain.style.left = '0';
+preloaderContain.style.top = '0';
+preloaderContain.style.left = '0';
 preloaderContain.style.display = 'flex';
 preloaderContain.style.justifyContent = 'center';
 preloaderContain.style.alignItems = 'center';
+
 document.body.appendChild(preloaderContain);
 
 const preloader = document.createElement('div');
 preloader.style.height = preloader.style.width = '70px';
-preloader.style.animation = 'rotation is infinite';
+//preloader.style.animation = 'rotation 1s linear infinite';
 preloaderContain.appendChild(preloader);
 
 const round = document.createElement('div');
@@ -19,18 +21,25 @@ round.style.border = '5px solid green';
 round.style.width = round.style.height = '70px';
 round.style.boxSizing = 'border-box';
 round.style.borderRadius = '100%';
+//round.style.backgroundColor = 'red';
 round.style.animation = 'rotation 1s linear infinite';
+
 preloader.appendChild(round);
 
 const square = document.createElement('div');
-square.style.height = square.style.width = '37px';
-square.style.backgroundColor = 'grey';
-square.style.opacity = '0,5'
+square.style.height = '50px'
+square.style.width = '35px'; // регулировка длины загрузки
+square.style.backgroundColor = 'black';
+square.style.display = 'flex';
+square.style.position = 'fixed';
+square.style.justifyContent = 'center center';
+square.style.alignItems = 'center center';
+square.style.margin = '20px'
 round.appendChild(square);
 
 document.addEventListener('DOMContentLoaded', function(){
-    const preloaderContain = document.createElement('preloaderContain');
-    preloaderContain.style.opacity = '0'
+    const preloader = document.getElementById('preloaderContain');
+    preloader.style.opacity = '0';
     setTimeout(function(){
         preloader.style.display = 'none';
     }, 300)
