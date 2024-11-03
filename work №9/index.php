c36179ec13af1aca3f7e3870afa2c49bb776283b
Chech <ivankuikov3333@gmail.com>
2024-11-03 03:31:04 +0300

    js

--- work №9/index.php
+++ work №9/index.php
@@ -7,14 +7,8 @@
     <title>Cvet</title>
 </head>
 <body>
-    <div id="preloaderContain">
-        <div id="preloader">
-            <div id="round"></div>
-            <div id="square"></div>
-        </div>
     </div>
     <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus quia omnis est, hic nihil placeat. Veritatis corrupti libero facilis numquam perspiciatis quos, vel, commodi labore suscipit, cum dignissimos odio cupiditate!</h1>
     <script src="scripts.js"></script>
-    <script src="js-cookie/dist/js.cookie.min.js"></script>
 </body>
 </html>
