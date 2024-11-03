4b381e8062f43e8888ca7a8b6c4446403627f652
Randee <pavelkusha@mail.ru>
2024-11-04 01:50:13 +0300

    modified:   "work \342\204\2269/scripts.js"

--- work №9/scripts.js
+++ work №9/scripts.js
@@ -1,4 +1,5 @@
 const preloaderContain = document.createElement('div');
+preloaderContain.id = 'preloaderContain'; // Присваиваем идентификатор
 preloaderContain.style.backgroundColor = 'black';
 preloaderContain.style.width = '100vw';
 preloaderContain.style.height = '100vh';
@@ -13,7 +14,6 @@ document.body.appendChild(preloaderContain);
 
 const preloader = document.createElement('div');
 preloader.style.height = preloader.style.width = '70px';
-//preloader.style.animation = 'rotation 1s linear infinite';
 preloaderContain.appendChild(preloader);
 
 const round = document.createElement('div');
@@ -21,13 +21,11 @@ round.style.border = '5px solid green';
 round.style.width = round.style.height = '70px';
 round.style.boxSizing = 'border-box';
 round.style.borderRadius = '100%';
-//round.style.backgroundColor = 'red';
-round.style.animation = 'rotation 1s linear infinite';
-
+round.style.animation = 'rotation 0.4s linear infinite'; // регулировка времени загрузки
 preloader.appendChild(round);
 
 const square = document.createElement('div');
-square.style.height = '50px'
+square.style.height = '50px' // регулировка ширины загрузки
 square.style.width = '35px'; // регулировка длины загрузки
 square.style.backgroundColor = 'black';
 square.style.display = 'flex';
@@ -37,10 +35,14 @@ square.style.alignItems = 'center center';
 square.style.margin = '20px'
 round.appendChild(square);
 
-document.addEventListener('DOMContentLoaded', function(){
+const styleSheet = document.createElement("style");
+styleSheet.tinnerText = `@keyframes rotation {0% { transform: rotate(0deg); }100% { transform: rotate(360deg); }}#preloaderContain {transition: opacity 0.3s ease;}`;
+document.head.appendChild(styleShee);
+
+document.addEventListener('DOMContentLoaded', function() {
     const preloader = document.getElementById('preloaderContain');
     preloader.style.opacity = '0';
-    setTimeout(function(){
+    setTimeout(function() {
         preloader.style.display = 'none';
-    }, 300)
-})
+    }, 300);
+});
